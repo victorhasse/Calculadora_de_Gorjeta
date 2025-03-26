9d01e13cf21750f150a6f9c88b6a07e8d8be25d7
victorhasse <inicial-folgado0j@icloud.com>
2025-03-26 19:17:56 -0300

    Initial commit

---
+++ Calculadora_de_Gorjeta.py
@@ -0,0 +1,6 @@
+print("Bem-vindo(a) a calculadora de gorgetas!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+result = (bill / people) * (tip / 100 + 1)
+print("Each person should pay: " + str(round(result, 2)))
